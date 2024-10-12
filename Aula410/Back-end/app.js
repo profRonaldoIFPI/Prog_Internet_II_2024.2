e9027c1dd3c90f15fe718379870a38e8966c4e5e
ronaldopborges <ronaldo.borges@alunos.ufersa.edu.br>
2024-10-12 16:07:13 -0300

    Adição do cors e incremento do id do usuário

--- Aula410/Back-end/app.js
+++ Aula410/Back-end/app.js
@@ -1,18 +1,14 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 const app = express();
 
 //converte o 'body' das requisições em json
 app.use(bodyParser.json()); //já inclui o URL-encoded 
 
+app.use(cors());
 //lista que será manipulada na API
-let usuarios = [
-    {
-        "id":"1",
-        "nome":"Ronaldo",
-        "senha":"1231245"
-    }
-]
+let usuarios = []
 
 //LISTA TODA
 app.get("/usuario",(req,res)=>{
@@ -22,9 +18,18 @@ app.get("/usuario",(req,res)=>{
 //ADICIONA ÍTEM A LISTA
 app.post("/usuario",(req,res)=>{
     const usuario = req.body
+    let novoId;
+    if (usuarios.length > 0) {
+        let utimoUsuario = usuarios[usuarios.length - 1];
+        novoId = (parseInt(utimoUsuario.id) + 1).toString();
+    } else {
+        novoId = '1';
+    }
+    usuario.id = novoId;
     usuarios.push(usuario)
     res.status(201).send("Usuário criado.")
 })
+
 //BUSCA UM ÍTEM NA LISTA PELO ID
 app.get("/usuario/:id",(req,res)=>{
     const id = req.params.id
@@ -52,11 +57,11 @@ app.delete("/usuario/:id", (req, res)=>{
 //o filtro retorna todos os itens da lista com o id diferente do informado
     usuarios = usuarios.filter(u => u.id !== id)
     
-//Forma alternativa
+//Outra forma
 //splice "fatia" a lista a partir do indice até o numero de ítens informado 
     //const index = usuarios.findIndex(u => u.id === id)
     //usuarios.splice(index, 1)
-    res.send("Usuário excluido")
+    res.send("Usuário excluido.")
 })
 
 app.listen(3000, (err)=>{
